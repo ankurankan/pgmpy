69d376797f55ae0d58029bfa4232aef019760757
Yidi Huang <huangy6@yahoo.com>
2018-03-29 03:38:17 -0400

    Save samples as tuples instead of numpy arrays

--- pgmpy/sampling/Sampling.py
+++ pgmpy/sampling/Sampling.py
@@ -416,7 +416,7 @@ class GibbsSampling(MarkovChain):
 
         types = [(var_name, "int") for var_name in self.variables]
         sampled = np.zeros(size, dtype=types).view(np.recarray)
-        sampled[0] = tuple([st for var, st in self.state])
+        sampled[0] = tuple(st for var, st in self.state)
         for i in tqdm(range(size - 1)):
             for j, (var, st) in enumerate(self.state):
                 other_st = tuple(st for v, st in self.state if var != v)
@@ -425,7 +425,7 @@ class GibbsSampling(MarkovChain):
                     self.transition_models[var][other_st],
                 )[0]
                 self.state[j] = State(var, next_st)
-            sampled[i + 1] = tuple([st for var, st in self.state])
+            sampled[i + 1] = tuple(st for var, st in self.state)
 
         return _return_samples(return_type, sampled)
 
