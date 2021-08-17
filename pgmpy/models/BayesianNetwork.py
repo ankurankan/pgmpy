cc0fa4615b566adb7f408e9bb41d348abdb47dae
Ankur Ankan <ankurankan@gmail.com>
2021-08-17 11:04:52 +0200

    Adds arguments to BayesianModel.simulate to be able to simulate using intervention, virtual intervention, evidence, virtual evidence

--- pgmpy/models/BayesianNetwork.py
+++ pgmpy/models/BayesianNetwork.py
@@ -1002,7 +1002,17 @@ class BayesianNetwork(DAG):
                 cpd.marginalize(cpd.variables[1:], inplace=True)
         return adj_model
 
-    def simulate(self, n_samples=10, include_latents=False, seed=None):
+    def simulate(
+        self,
+        n_samples=10,
+        do=None,
+        evidence=None,
+        virtual_evidence=None,
+        virtual_intervention=None,
+        include_latents=False,
+        seed=None,
+        show_progress=True,
+    ):
         """
         Simulates data from the given model. Internally uses
         BayesianModelSampling.forward_sample to generate the data.
@@ -1032,9 +1042,82 @@ class BayesianNetwork(DAG):
         from pgmpy.sampling import BayesianModelSampling
 
         self.check_model()
-        return BayesianModelSampling(self).forward_sample(
-            size=n_samples, include_latents=include_latents, seed=seed
+        model = self.copy()
+
+        evidence = {} if evidence is None else evidence
+        do = {} if do is None else do
+        virtual_intervention = (
+            [] if virtual_intervention is None else virtual_intervention
         )
+        virtual_evidence = [] if virtual_evidence is None else virtual_evidence
+
+        if set(do.keys()).intersection(set(evidence.keys())):
+            raise ValueError("Variable can't be in both do and evidence")
+
+        # Step 1: If do or virtual_intervention is specified, modify the network structure.
+        if (do != {}) or (virtual_intervention != []):
+            virt_nodes = [cpd.variables[0] for cpd in virtual_intervention]
+            model = model.do(list(do.keys()) + virt_nodes)
+            evidence = {**evidence, **do}
+            virtual_evidence = [*virtual_evidence, *virtual_intervention]
+
+        # Step 2: If virtual_evidence; modify the network structure
+        if virtual_evidence != []:
+            for cpd in virtual_evidence:
+                var = cpd.variables[0]
+                if var not in model.nodes():
+                    raise ValueError(
+                        "Evidence provided for variable which is not in the model"
+                    )
+                elif len(cpd.variables) > 1:
+                    raise (
+                        "Virtual evidecne should be defined on individual variables. Maybe your are looking for soft evidence."
+                    )
+                elif self.get_cardinality(var) != cpd.get_cardinality([var])[var]:
+                    raise ValueError(
+                        "The number of states/cardinality for the evideence should be same as the nubmer fo states/cardinalit yof teh variable in the model"
+                    )
+
+            for cpd in virtual_evidence:
+                var = cpd.variables[0]
+                new_var = "__" + var
+                model.add_edge(var, new_var)
+                values = np.vstack((cpd.values, 1 - cpd.values))
+                new_cpd = TabularCPD(
+                    variable=new_var,
+                    variable_card=2,
+                    values=values,
+                    evidence=[var],
+                    evidence_card=[model.get_cardinality(var)],
+                    state_names={new_var: [0, 1], var: cpd.state_names[var]},
+                )
+                model.add_cpds(new_cpd)
+                evidence[new_var] = 0
+
+        # Step 3: If no evidence do a forward sampling
+        if evidence is None:
+            samples = BayesianModelSampling(model).forward_sample(
+                size=n_samples,
+                include_latents=include_latents,
+                seed=seed,
+                show_progress=show_progress,
+            )
+
+        # Step 4: If evidence; do a rejection sampling
+        else:
+            samples = BayesianModelSampling(model).rejection_sample(
+                size=n_samples,
+                evidence=[(k, v) for k, v in evidence.items()],
+                include_latents=include_latents,
+                seed=seed,
+                show_progress=show_progress,
+            )
+
+        # Step 5: Postprocess and return
+        if include_latents:
+            return samples
+        else:
+            return samples.loc[:, set(self.nodes()) - self.latents]
 
     def save(self, filename, filetype="bif"):
         """
